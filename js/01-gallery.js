a96f5b3d06ec55d6ccbeb6a6092cf8de7c12d36b
OttoCarius <ottocarius1993@gmail.com>
2022-07-30 14:58:37 +0300

    ..

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const markup = galleryItems.map((el) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" onclick="event.preventDefault()" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>
+`;
+});
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+
+gallery.addEventListener("click", (e) => {
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.sourse}" width="800" height="600">
+`);
+
+  instance.show();
+});
